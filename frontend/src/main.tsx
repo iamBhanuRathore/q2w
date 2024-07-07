74b9876fe8b0683c38f4d95781c8e785c404737b
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2024-07-07 22:18:33 +0530

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,9 @@ import "./index.css";
 import { ThemeProvider } from "./provider/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <ThemeProvider defaultTheme="dark" storageKey="q2w-theme-ui">
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="q2w-theme-ui">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
